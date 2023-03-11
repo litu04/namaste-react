b611c16d8bb4a30350dc4b48f9f9a360fd1ab5c8
litu04 <debadattakar96@gmail.com>
2023-03-11 20:04:42 +0530

    laying the foundation

--- practice2/app.js
+++ practice2/app.js
@@ -1,19 +1,61 @@
 import React from "react"
-import ReactDOM from "react-dom"
-const heading1 = React.createElement("h1",
-{
-    id: "title",
-},"Heading 1 in React")
-        const heading2 = React.createElement("h2",
-        {
-            id: "title",
-        },"Heading 2 in React")
-        const heading3 = React.createElement("h3",
-        {
-            id: "title",
-        },"Heading 3 in React")
-        const container = React.createElement("div", {
-            id: "container",
-        }, [heading1,heading2,heading3])
-        const root = ReactDOM.createRoot(document.getElementById("root"))
-        root.render(container)
+import ReactDOM from "react-dom/client"
+
+{/* <div>Namsate React</div>
+<ul>
+    <li>Home</li>
+    <li>Review/li>
+    <li>Contact</li>
+    <li>About</li>
+</ul> */}
+
+// const container =
+// [
+//     React.createElement("div",{className:"container"},"Namaste React"),
+//     React.createElement("ul",{className:"container"},[
+//         React.createElement("li",{},"Home"),
+//         React.createElement("li",{},"Review"),
+//         React.createElement("li",{},"Contact"),
+//         React.createElement("li",{},"About")
+//     ])
+// ]
+//React Element
+// const title = (
+//         <h1 id="title">Namaste React</h1>
+// )
+// const HeaderComp = () => {
+//     return (
+//         <>
+//             {title}
+//             <h2>H2 tag</h2>
+//             <p>Hello Ji</p>
+//         </>
+//     )
+// }
+// const Title = () => (
+//     <h1 id="title">Namaste React</h1>
+// )
+// const HeaderComp = () => {
+// return (
+//     <>
+//         {Title()}
+//         <h2>H2 tag</h2>
+//         <p>Hello Ji, pushpa</p>
+//     </>
+// )
+// }
+const Title = () => (
+    <h1 id="title">Namaste React</h1>
+)
+const HeaderComp = () => {
+return (
+    <>
+        <Title/>  
+        <h3>H3 tag</h3>
+        <p>Good Evening, John</p>
+    </>
+)
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<HeaderComp/>)
---
+++ practice2/app2.js
@@ -0,0 +1,23 @@
+import React from "react"
+import ReactDOM from "react-dom"
+const heading1 = React.createElement("h1",
+{
+    id: "title",
+},"Heading 1 in React")
+
+const heading2 = React.createElement("h2",
+{
+    id: "title",
+},"Heading 2 in React")
+
+const heading3 = React.createElement("h3",
+{
+    id: "title",
+},"Heading 3 in React")
+
+const container = React.createElement("div", {
+    id: "container",
+}, [heading1,heading2,heading3])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(container)
