8d159cc7d808fdef7209662e00f6c6cda22ba22b
litu04 <debadattakar96@gmail.com>
2023-03-15 02:39:27 +0530

    Search working with constant restaurants

--- Food_app/src/components/Body.js
+++ Food_app/src/components/Body.js
@@ -5,15 +5,15 @@ import RestaurantCard from "./RestaurantCard"
 const Body = () => {
   const [restaurants, setRestaurants] = useState(restaurantList)
   const [searchText,setSearchText] = useState("")
-  const filterData = (text,restaurants) => {
-    const data = restaurants.filter((restaurant) => restaurant.data.name.includes(text))
+  const filterData = (text,restaurantList) => {
+    const data = restaurantList.filter((restaurant) => restaurant.data.name.includes(text))
     setRestaurants(data)
   }
   return (
     <>
       <div className='search-container'>
         <input className="search-input" value={searchText} placeholder="Search here..." onChange={(e) => setSearchText(e.target.value)}/>
-        <button className="search-btn" onClick={() => filterData(searchText, restaurants)}>Search</button>
+        <button className="search-btn" onClick={() => filterData(searchText, restaurantList)}>Search</button>
       </div>
       <div className="restaurant-list">
           {restaurants.map((restaurant) => <RestaurantCard key={restaurant.data.id} {...restaurant.data} />)}
